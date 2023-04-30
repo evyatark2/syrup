c19b0026aad1dd3606e46f1db8de21e0a4872264
Evyatar Stalinsky <evyatar@stalinsky.xyz>
2023-04-30 21:59:57 +0300

    Fix Rain

--- script/npc/12101.lua
+++ script/npc/12101.lua
@@ -6,11 +6,15 @@ function talk(c)
         end
 
         if i == 1 then
-            i = i + c:sendNextPrev("If you want to get stronger, then go to #bSouthperry#k where there's a harbor. Ride on the gigantic ship and head to the place called #bVictoria Island#k. It's incomparable in size compared to this tiny island.")
+            i = i + c:sendPrevNext("If you want to get stronger, then go to #bSouthperry#k where there's a harbor. Ride on the gigantic ship and head to the place called #bVictoria Island#k. It's incomparable in size compared to this tiny island.")
         end
 
         if i == 2 then
             i = i + c:sendPrev("At the Victoria Island, you can choose your job. Is it called #bPerion#k...? I heard there's a bare, desolate town where warriors live. A highland...what kind of a place would that be?")
+        end
+
+        if i == 3 then
+            break
         end
     end
 end
