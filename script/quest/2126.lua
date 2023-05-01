ca4b1c4091c58988489be9c9d23946d3f14bcded
Evyatar Stalinsky <evyatar@stalinsky.xyz>
2023-05-01 15:51:32 +0300

    Implement 'A Supply from the Sand Crew'

---
+++ script/quest/2126.lua
@@ -0,0 +1,11 @@
+-- [[ 2126 - A Supply from the Sand Crew! ]]
+
+function end_(c)
+    if c:hasItem(4031619) then
+        c:sendOk("Oh, you brought #p2012019#'s box! Thank you.")
+        c:gainItems({ { id = 4031619, amount = -1 } })
+        c:endQuestNow()
+    else
+        c:sendOk("Please bring me the box with the supplies that lies with #b#p2012019##k...")
+    end
+end
